27744083fa2e96bab810b36269354df5a0c56b05
Simone Bacchio <s.bacchio@gmail.com>
2020-08-28 20:19:38 +0300

    Adding tests for logical

---
+++ test/test_logical.py
@@ -0,0 +1,21 @@
+from lyncs_utils import isiterable, single_true
+
+
+def test_isiterable():
+    assert isiterable([])
+    assert isiterable(map(lambda _: _, []))
+    assert isiterable([1, 2, 3], int)
+    assert not isiterable(1)
+    assert not isiterable([1, 2, 3], (str, float))
+
+
+def test_single_true():
+    assert single_true([1])
+    assert single_true([1, 0, 0, 0])
+    assert single_true([True, False, 0, 0])
+    assert not single_true([False, True, False, True])
+    assert not single_true(
+        [
+            False,
+        ]
+    )
