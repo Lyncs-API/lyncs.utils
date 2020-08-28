43de38ed00c94a9dee895e57e254c167bceb4da4
Simone Bacchio <s.bacchio@gmail.com>
2020-08-28 17:46:36 +0300

    Adding test for factors

---
+++ test/test_factors.py
@@ -0,0 +1,8 @@
+from lyncs_utils import prime_factors, factors, prod
+
+
+def test_factors():
+    nums = [1, 20, 30, 123, 211]
+    for num in nums:
+        assert prod(prime_factors(num)) == num
+        assert prod(factors(num)) % num == 0
