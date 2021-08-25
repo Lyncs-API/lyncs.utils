1cb8276ce273b22c4731f18c0fdf698a1110e353
Simone Bacchio <s.bacchio@gmail.com>
2021-08-25 05:42:04 +0300

    Removing unnecessary dependencies

--- setup.py
+++ setup.py
@@ -3,10 +3,7 @@ from lyncs_setuptools import setup
 setup(
     "lyncs_utils",
     install_requires=[
-        "lyncs_setuptools",
-        "gitpython",
-        "cmake",
         "numpy",
     ],
-    extras_require={"test": ["pytest", "pytest-cov", "ipython"]},
+    extras_require={"test": ["pytest", "pytest-cov", "ipython", "numpy", "lyncs_setuptools"]},
 )
