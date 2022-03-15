8f20caa3ae31ed99ca6e0c602eaab55714194933
Simone Bacchio <s.bacchio@gmail.com>
2022-03-15 13:46:20 +0200

    Adding dbdict

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
     "lyncs_utils",
     extras_require={
         "numpy": ["numpy"],
+        "dbdict": ["sqlite3"],
         "test": ["pytest", "pytest-cov", "ipython", "numpy", "lyncs_setuptools"],
     },
 )
