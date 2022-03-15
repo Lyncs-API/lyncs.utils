1541777cbc625f79a205b1d9b5cae62cbc7165c8
Simone Bacchio <s.bacchio@gmail.com>
2022-03-15 14:17:56 +0200

    Fixing setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     "lyncs_utils",
     extras_require={
         "numpy": ["numpy"],
-        "dbdict": ["sqlite3"],
+        "dbdict": ["sqlite"],
         "test": ["pytest", "pytest-cov", "ipython", "numpy", "lyncs_setuptools"],
     },
 )
