2f8042daed93860b971c07a30dcb629bb6941b0a
Simone Bacchio <s.bacchio@gmail.com>
2024-03-22 09:30:00 +0200

    Fixing failing test

--- test/test_math.py
+++ test/test_math.py
@@ -10,7 +10,7 @@ def test_iscomplex():
 def test_isclose():
     assert isclose(1, 1.0001, abs_tol=0.0001)
     assert not isclose(1, 1.0002, abs_tol=0.0001)
-    with warns(None):
+    with warns():
         assert isclose(1, 1.01, warn_tol=0.1)
     assert isclose(1j, 1.0001j, abs_tol=0.0001)
     assert isclose(1j, 1.0001j, rel_tol=0.0001)
@@ -29,4 +29,4 @@ def test_factors():
     for num in nums:
         assert prod(prime_factors(num)) == num
         assert prod(factors(num)) % num == 0
-        assert prod(prime_factors(num**2)) == num**2
+        assert prod(prime_factors(num ** 2)) == num ** 2
